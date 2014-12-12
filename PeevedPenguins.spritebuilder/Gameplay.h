08427e72e0b7e7950c11f00734737a971b2ba756
bfallon <brian@brianfallon.com>
2014-12-11 20:50:40 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Gameplay.h
@@ -0,0 +1,13 @@
+//
+//  Gameplay.h
+//  PeevedPenguins
+//
+//  Created by Brian Fallon on 12/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Gameplay : CCNode
+
+@end
