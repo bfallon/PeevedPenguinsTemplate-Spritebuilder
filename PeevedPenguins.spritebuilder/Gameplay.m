7f230c11b21d41d8d7aa82cace7bc37a6c5e50b5
bfallon <brian@brianfallon.com>
2014-12-11 21:56:55 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -88,19 +88,17 @@
         // releases the joint and lets the catapult snap back
         [_mouseJoint invalidate];
         _mouseJoint = nil;
-        // create a penguin from the ccb-file
-        _currentPenguin = [CCBReader load:@"Penguin"];
-        // initially position it on the scoop. 34,138 is the position in the node space of the _catapultArm
-        CGPoint penguinPosition = [_catapultArm convertToWorldSpace:ccp(34, 138)];
-        // transform the world position to the node space to which the penguin will be added (_physicsNode)
-        _currentPenguin.position = [_physicsNode convertToNodeSpace:penguinPosition];
-        // add it to the physics world
-        [_physicsNode addChild:_currentPenguin];
-        // we don't want the penguin to rotate in the scoop
-        _currentPenguin.physicsBody.allowsRotation = FALSE;
+        
+        // releases the joint and lets the penguin fly
+        [_penguinCatapultJoint invalidate];
+        _penguinCatapultJoint = nil;
 
-        // create a joint to keep the penguin fixed to the scoop until the catapult is released
-        _penguinCatapultJoint = [CCPhysicsJoint connectedPivotJointWithBodyA:_currentPenguin.physicsBody bodyB:_catapultArm.physicsBody anchorA:_currentPenguin.anchorPointInPoints];
+        // after snapping rotation is fine
+        _currentPenguin.physicsBody.allowsRotation = TRUE;
+
+        // follow the flying penguin
+        CCActionFollow *follow = [CCActionFollow actionWithTarget:_currentPenguin worldBoundary:self.boundingBox];
+        [_contentNode runAction:follow];
     }
 }
 
