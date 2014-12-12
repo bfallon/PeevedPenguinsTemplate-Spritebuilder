0dc387ab94862c2a351358b3746032f5bdd58773
bfallon <brian@brianfallon.com>
2014-12-11 22:04:57 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -30,6 +30,8 @@
     // nothing shall collide with our invisible nodes
     _pullbackNode.physicsBody.collisionMask = @[];
     _mouseJointNode.physicsBody.collisionMask = @[];
+
+    _physicsNode.collisionDelegate = self;
 }
 
 // called on every touch in this scene
@@ -84,7 +86,6 @@
 - (void)releaseCatapult {
     if (_mouseJoint != nil)
     {
-            CCLOG(@"FFF");
         // releases the joint and lets the catapult snap back
         [_mouseJoint invalidate];
         _mouseJoint = nil;
@@ -127,6 +128,11 @@
 - (void)retry {
     // reload this level
     [[CCDirector sharedDirector] replaceScene: [CCBReader loadAsScene:@"Gameplay"]];
+}
+
+-(void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB
+{
+    CCLOG(@"Something collided with a seal!");
 }
 
 @end
