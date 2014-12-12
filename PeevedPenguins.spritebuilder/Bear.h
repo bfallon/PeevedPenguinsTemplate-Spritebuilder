5b5e5d064500656ea628c0050ccb28f5535ce9cc
bfallon <brian@brianfallon.com>
2014-12-11 20:43:22 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Bear.h
@@ -0,0 +1,13 @@
+//
+//  Bear.h
+//  PeevedPenguins
+//
+//  Created by Brian Fallon on 12/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "CCNode.h"
+
+@interface Bear : CCNode
+
+@end
---
+++ PeevedPenguins.spritebuilder/Bear.m
@@ -0,0 +1,13 @@
+//
+//  Bear.m
+//  PeevedPenguins
+//
+//  Created by Brian Fallon on 12/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Bear.h"
+
+@implementation Bear
+
+@end
