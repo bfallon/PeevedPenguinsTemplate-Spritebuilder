297fb20da5f0ecf89e3a74bddd917c23744c2895
bfallon <brian@brianfallon.com>
2014-12-11 20:25:38 -0500

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,21 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Brian Fallon on 12/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+- (id)init {
+    self = [super init];
+
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+
+    return self;
+}
+@end
