9b125395dd9664e28f5fca170f8c7f88a5918be6
bfallon <brian@brianfallon.com>
2014-12-11 21:34:25 -0500

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Gameplay.m
+++ PeevedPenguins.spritebuilder/Gameplay.m
@@ -21,7 +21,7 @@
     [_levelNode addChild:level];
 
     // visualize physics bodies & joints
-//    _physicsNode.debugDraw = TRUE;
+        _physicsNode.debugDraw = TRUE;
 }
 
 // called on every touch in this scene
